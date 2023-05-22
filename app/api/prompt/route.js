b836a288cd1157e2e7dbc54adbdbafa9b8ae1331
Joseph Schafer <joseph@schafers.com>
2023-05-21 21:20:27 -0600

    copy and paste of original file

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -1,23 +1,14 @@
+import Prompt from "@models/prompt";
 import { connectToDB } from "@utils/database";
-import Prompt from "@models/models";
 
-export const GET = async (request, { params }) => {
-  try {
-    await connectToDB();
-    const prompts = await Prompt.find( {} )
-      .populate('creator')
-      .sort({ createdAt: 1 });
+export const GET = async (request) => {
+    try {
+        await connectToDB()
 
-    return new Response(JSON.stringify(prompts), {
-      status: 200,
-      headers: {
-        'cache-control': 's-maxage=1, stale-while-revalidate=59',
-      }
-    });
-  } catch (error) {
-    console.log(error);
-    return new Response("Failed to fetch all prompts", {
-      status: 500
-    })
-  }
-}
+        const prompts = await Prompt.find({}).populate('creator').sort({createdAt: 1})
+
+        return new Response(JSON.stringify(prompts), { status: 200 })
+    } catch (error) {
+        return new Response("Failed to fetch all prompts", { status: 500 })
+    }
+} 
