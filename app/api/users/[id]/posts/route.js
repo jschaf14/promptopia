8e27ceba1109a26f2baf7a8a4f81c9657cc12b85
Joseph Schafer <joseph@schafers.com>
2023-05-21 21:21:04 -0600

    fix

--- app/api/users/[id]/posts/route.js
+++ app/api/users/[id]/posts/route.js
@@ -1,14 +1,20 @@
-import Prompt from "@models/prompt";
 import { connectToDB } from "@utils/database";
+import Prompt from "@models/models";
 
-export const GET = async (request) => {
-    try {
-        await connectToDB()
+export const GET = async (req, { params }) => {
+  try {
+    await connectToDB();
+    const prompts = await Prompt.find({
+      creator: params.id
+    }).populate('creator');
 
-        const prompts = await Prompt.find({}).populate('creator')
-
-        return new Response(JSON.stringify(prompts), { status: 200 })
-    } catch (error) {
-        return new Response("Failed to fetch all prompts", { status: 500 })
-    }
+    return new Response(JSON.stringify(prompts), {
+      status: 200
+    })
+  } catch (error) {
+    console.log(error);
+    return new Response("Failed to fetch all prompts", {
+      status: 500
+    })
+  }
 }
