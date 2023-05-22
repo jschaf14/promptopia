be9335dc6ec0a4f1824d0d60faabc7e482607ec5
Joseph Schafer <joseph@schafers.com>
2023-05-21 21:26:56 -0600

    attempting copy and pase of route.js

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -1,23 +1,14 @@
+import Prompt from "@models/prompt";
 import { connectToDB } from "@utils/database";
-import Prompt from "@models/models";
 
-export const GET = async (request, { params }) => {
+export const GET = async (request) => {
   try {
-    await connectToDB();
-    const prompts = await Prompt.find( {} )
-      .populate('creator')
-      .sort({ createdAt: 1 });
+    await connectToDB()
 
-    return new Response(JSON.stringify(prompts), {
-      status: 200,
-      headers: {
-        'cache-control': 's-maxage=1, stale-while-revalidate=59',
-      }
-    });
+    const prompts = await Prompt.find({}).populate('creator')
+
+    return new Response(JSON.stringify(prompts), { status: 200 })
   } catch (error) {
-    console.log(error);
-    return new Response("Failed to fetch all prompts", {
-      status: 500
-    })
+    return new Response("Failed to fetch all prompts", { status: 500 })
   }
-}
+} 
